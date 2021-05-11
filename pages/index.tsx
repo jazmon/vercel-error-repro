7ab0c1dd9b77efadca3321ee799f3b37934c1752
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2021-05-11 10:12:07 +0100

    Remove even morerer stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { Flex, Heading } from 'theme-ui'
+import { Flex } from 'theme-ui'
 
 export default function Home(): JSX.Element {
   return (
@@ -8,34 +8,8 @@ export default function Home(): JSX.Element {
         <title id="home-title">Site</title>
         <meta name="description" content="Template Site" />
       </Head>
-
       <Flex as="main" id="main">
-        <Heading
-          as="h1"
-          sx={{
-            position: 'absolute',
-            width: '1px',
-            height: '1px',
-            margin: -'1px',
-            border: '0',
-            padding: '0',
-            clip: 'rect(0 0 0 0)',
-            overflow: 'hidden',
-          }}
-        >
-          Title
-        </Heading>
-
-        <div className="container">
-          <Head>
-            <title>Create Next App</title>
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-
-          <main>
-            <div sx={{ color: (t) => t.colors.background }}>Hello world</div>
-          </main>
-        </div>
+        <div sx={{ color: (t) => t.colors.background }}>Hello world</div>
       </Flex>
     </Flex>
   )
